3141d402b8f3bfa48275b49c0e318fe02ba3a15a
elekchen9804 <y.h.chen2016@gmail.com>
2019-01-31 16:59:42 +0800

    Add file compare and move feature

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const remote = require('electron').remote;
 const fs = remote.require('fs-extra');
 const readdirp = require('readdirp');
+const path = require('path');
+const crypto = require('crypto');
 const dialog = remote.dialog;
 
 $(function () {
@@ -35,10 +37,53 @@ $(function () {
             })();
         });
 
-        // 儲存設定
-        // $('.setting-save').on('click', async () => {
-        //     await editFile(setting);
-        // });
+        // 建立工作區：複製靜態版至Ｍodify, 建立備份檔
+        $('#start-work').on('click', function () {
+            let siteName = $('#site-name').val();
+            let designPath = path.join(elemList.designPath, siteName);
+            let modifySiteNamePath = path.join(elemList.modifyPath, siteName);
+            let modifySiteNameBackupPath = path.join(elemList.modifyPath, 'Backup');
+
+            (async () => {
+                updateEleValue({ workStatus: '努力搬運中...' });
+                // 清空工作區
+                await clearDir(elemList.modifyPath);
+                // 複製靜態站到工作區
+                await copyFile(designPath, modifySiteNamePath);
+                // 產生備份檔
+                await copyFile(designPath, modifySiteNameBackupPath);
+                updateEleValue({ workStatus: '完成搬運與備份！' });
+            })()
+        });
+
+        // 搬移至版空：比對差異檔案，複製到三個地方
+        $('#finish-work').on('click', function () {
+            let siteName = $('#site-name').val();
+            let designPath = path.join(elemList.designPath, siteName);
+            let sitePath = path.join(elemList.sitePath, siteName);
+            let programPath = path.join(elemList.programPath, siteName);
+            let modifySiteNamePath = path.join(elemList.modifyPath, siteName);
+            let modifySiteNameBackupPath = path.join(elemList.modifyPath, 'Backup');
+            let modifySiteNameDistPath = path.join(elemList.modifyPath, 'Dist');
+
+            (async () => {
+                updateEleValue({ workStatus: '複製檔案至版控中...' });
+                // 取得所有檔案清單
+                let allModifyFileList = await listAllFilePath(modifySiteNamePath);
+                // 取得異動檔案清單
+                const resultFileList = listModifyFilePath(allModifyFileList, modifySiteNamePath, modifySiteNameBackupPath);
+                // 產出異動檔案包
+                await exportDist(resultFileList, modifySiteNamePath, modifySiteNameDistPath);
+                // Dist to design 
+                await copyFile(modifySiteNameDistPath, designPath);
+                // Dist to site
+                await copyFile(modifySiteNameDistPath, sitePath);
+                // Dist to program (only css and img)
+                await copyFile(path.join(modifySiteNameDistPath, 'Content'), path.join(programPath, 'Content'));
+
+                updateEleValue({ workStatus: '檔案已成功搬至版控！' });
+            })();
+        });
     }
 });
 
@@ -52,8 +97,6 @@ function getSetting() {
 // Update Element value
 function updateEleValue(obj) {
     for (let key in obj) {
-        console.log('key', key);
-        console.log('obj', obj[key]);
         if (obj.hasOwnProperty(key)) {
             $(`[data-bind=${key}]`).html(obj[key]);
         }
@@ -88,6 +131,18 @@ function readFile(filepath) {
 
 }
 
+// 刪除 Dir
+function clearDir(dir) {
+    return new Promise((resolve, reject) => {
+        fs.emptyDir(dir, err => {
+            if (err) {
+                reject(console.log(err));
+            }
+            resolve();
+        });
+    })
+}
+
 // copy files
 function copyFile(source, target) {
     return new Promise((resolve, reject) => {
@@ -111,5 +166,93 @@ function editFile(data) {
             }
             resolve();
         });
+    })
+}
+
+// 取得hash值
+function getFileHash(filePath) {
+    let buffer = fs.readFileSync(filePath);
+    return crypto.createHash('sha1').update(buffer).digest('hex');
+}
+
+// 列出所有檔案
+function listAllFilePath(rootPath) {
+    return new Promise((resolve, reject) => {
+        // 所有檔案路徑
+        let rawFilePaths = [];
+        readdirp({
+            root: rootPath,
+            entryType: 'files',
+            fileFilter: ['!gulpfile.js',
+                '!package.json',
+                '!*.asax',
+                '!*.config',
+                '!App_Data',
+                '!CdnRedirect',
+                '!Cdn2Redirect',
+                '!node_modules',
+                '!fonts']
+        },
+            // This callback is executed everytime a file or directory is found inside the providen path
+            fileInfo => {
+                // parentDir : directory in which entry was found (relative to given root)
+                // fullParentDir : full path to parent directory
+                // name : name of the file/directory
+                // path : path to the file/directory (relative to given root)
+                // fullPath : full path to the file/directory found
+                // stat : built in stat object
+
+                rawFilePaths.push(
+                    fileInfo.path
+                );
+            },
+
+            // This callback is executed once 
+            (err) => {
+
+                if (err) {
+                    reject(console.log(err));
+                } else {
+                    resolve(rawFilePaths);
+                }
+            }
+        );
+    })
+}
+
+// 比對 Modify 與 backup 檔案
+function listModifyFilePath(filePathArray, modifyPath, backupPath) {
+    // 篩選後檔案
+    let resultFileList = [];
+
+    // 篩選出有被改過的檔案
+    resultFileList = filePathArray.filter(value => {
+        let sourceFilePath = path.join(modifyPath, value);
+        let backupFilePath = path.join(backupPath, value);
+
+        // 若備份檔不存在, 表示是新增檔案
+        try {
+            fs.statSync(backupFilePath);
+            return getFileHash(sourceFilePath) !== getFileHash(backupFilePath);
+        }
+        catch (err) {
+            return true;
+        }
+    });
+
+    return resultFileList;
+}
+
+// 複製檔案到Dist
+function exportDist(pathArray, sourcePath, distPath) {
+    return new Promise((resolve, reject) => {
+        for (let i = 0; i < pathArray.length; i++) {
+            let resultFilePath = path.join(sourcePath, pathArray[i]);
+            let distFilePath = path.join(distPath, pathArray[i]);
+
+            fs.copySync(resultFilePath, distFilePath);
+        }
+
+        resolve();
     })
 }

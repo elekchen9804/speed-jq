667893bc84ec1dc38fcd954d50e4d68b45b7f881
elekchen9804 <y.h.chen2016@gmail.com>
2019-02-19 22:57:26 +0800

    Add Electron-Packager, but is not work yet

--- main.js
+++ main.js
@@ -54,3 +54,51 @@ app.on('activate', () => {
 
 // 你可以在這個檔案中繼續寫應用程式主程序要執行的程式碼。 
 // 你也可以將它們放在別的檔案裡，再由這裡 require 進來。
+
+// For exe setup
+var handleStartupEvent = function () {
+    if (process.platform !== 'win32') {
+        return false;
+    }
+
+    var squirrelCommand = process.argv[1];
+
+    switch (squirrelCommand) {
+        case '--squirrel-install':
+        case '--squirrel-updated':
+            install();
+            return true;
+        case '--squirrel-uninstall':
+            uninstall();
+            app.quit();
+            return true;
+        case '--squirrel-obsolete':
+            app.quit();
+            return true;
+    }
+
+    function install() {
+        var cp = require('child_process');
+        var updateDotExe = path.resolve(path.dirname(process.execPath), '..', 'update.exe');
+        var target = path.basename(process.execPath);
+        var child = cp.spawn(updateDotExe, ["--createShortcut", target], { detached: true });
+        child.on('close', function (code) {
+            app.quit();
+        });
+    }
+
+    function uninstall() {
+        var cp = require('child_process');
+        var updateDotExe = path.resolve(path.dirname(process.execPath), '..', 'update.exe');
+        var target = path.basename(process.execPath);
+        var child = cp.spawn(updateDotExe, ["--removeShortcut", target], { detached: true });
+        child.on('close', function (code) {
+            app.quit();
+        });
+    }
+
+};
+
+if (handleStartupEvent()) {
+    return;
+}
